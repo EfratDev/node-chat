ae0fb9cf3e3507e7c196585f7376cf7534f016ba
FlowerPower2 <bibifeldman@gmail.com>
2019-10-22 12:07:03 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+
+io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '* <i>' + socket.username + ' has joined the chat</i>');
+    });
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '* <i>' + socket.username + ' has disconnected</i>');
+    });
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>:' + message);
+    });
+});
+
+
+const server = http.listen(8080, function() {
+    console.log('listening on *:8080');
+});
